e1cd00c9233c1b168df0e243063d7e5284ddd056
Yogendra Kumar <yk18011999@gmail.com>
2026-09-06 17:25:23 +0530

    add objet

--- .sfdx/typings/lwc/customlabels.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@salesforce/label/c.Test" {
-    var Test: string;
-    export default Test;
-}
-declare module "@salesforce/label/c.Test2" {
-    var Test2: string;
-    export default Test2;
-}
